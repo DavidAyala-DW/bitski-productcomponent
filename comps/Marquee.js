580aab3ca13fbe576b29f7149280ff2b2d42604d
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-03-29 12:07:12 -0500

    added scrolling text

--- comps/Marquee.js
+++ comps/Marquee.js
@@ -1,33 +1,68 @@
+import {useEffect, useState} from "react";
 import styles from "../styles/marquee.module.scss";
 
 const marqueeText = process.env.NEXT_PUBLIC_HERO_MARQUEE_TEXT;
 
-let res = Array.from({ length: 10 }, (_, i) => (
+let res = Array.from({ length: 20 }, (_, i) => (
   <span  key={i}>
     <span>
-        {marqueeText}
-    </span>
-    <span className="text-outline px-4 ">
         {marqueeText}
     </span>
   </span>
 ));
 
 const Marquee = () => {
+
+  const [scroll, setScroll] = useState(typeof window !== "undefined" ? window.scrollY : 0);
+  
+  useEffect(() => {
+
+    function  handleScrollEvent() {
+
+      if(typeof window !== "undefined"){
+        if(window.scrollY*0.5 <= 1300){
+          setScroll(window.scrollY);
+        }
+      }
+
+    }
+
+    document.addEventListener("scroll",handleScrollEvent);
+
+    return document.removeEventListener("scroll",handleScrollEvent,true);
+    
+  }, [] );
+
   return (
     <>
-      <div className="text-3xl">
+
+      <style jsx>
+        {
+          `
+            .scroll1 {
+              transform: translateX(-${scroll*0.5}px);
+            }
+            .scroll2 {
+              transform: translateX(${scroll*0.5}px);
+            }
+          `
+        }
+      </style>
+
+      <div className="text-3xl max-w-[2500px] mx-auto">
         <div className="primary-color text-primary  h-[37px] md:h-[50px]">
+
           <div className={styles.marquee}>
             <div className={styles.track}>
-              <div className={styles.content}>{res}</div>
+              <div className={styles.content + ` transition duration-75 ease-linear scroll1`}>{res}</div>
             </div>
           </div>
+
         </div>
         <div className="secondary-color marqee2 h-[37px] md:h-[50px]">
-          <div className={styles.marquee}>
+          <div className={styles.marquee + " flex justify-center"}>
             <div className={styles.trackReverse}>
-              <div className={styles.content}>{res} </div>
+              <div className={styles.content + ` transition duration-75 ease-linear scroll2`}>{res} </div>
             </div>
           </div>
         </div>
