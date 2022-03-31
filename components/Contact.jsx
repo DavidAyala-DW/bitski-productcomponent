a3280599292da44462ac5bf0909758777036e829
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-03-31 10:21:29 -0500

    module -> arraySocialMedia and change all component js to jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,51 +1,15 @@
-import Image from "next/image";
 import {useEffect, useState} from 'react'
+import Image from "next/image";
 import Grid from "../public/footer_grid.svg";
-import Twitter from "../public/twitter.svg";
-import Instagram from "../public/instagram.svg";
-import Discord from "../public/discord.svg";
-import Tiktok from "../public/tiktok.svg";
-import Telegram from "../public/telegram.svg";
-import Reddit from "../public/reddit.svg";
+import { arraySocialMedia } from '../helpers';
+
 const footerImage = "/" + process.env.NEXT_PUBLIC_MAIN_IMAGE;
 
-function Main() {
+function Contact() {
 
   const [height, setHeight] = useState(0);
   const [width, setWidth] = useState(0);
   const [isLoadImage, setIsLoadImage] = useState(false);
-
-  const arraySocialMedia = [
-
-    {
-      Icon: Twitter,
-      URL: process.env.NEXT_PUBLIC_TWITTER_LINK
-    },
-
-    {
-      Icon: Instagram,
-      URL: process.env.NEXT_PUBLIC_INSTAGRAM_LINK
-    },
-
-    {
-      Icon: Discord,
-      URL: process.env.NEXT_PUBLIC_DISCORD_LINK
-    },
-    {
-      Icon: Tiktok,
-      URL: process.env.NEXT_PUBLIC_TIKTOK_LINK
-    },
-    {
-      Icon: Telegram,
-      URL: process.env.NEXT_PUBLIC_TELEGRAM_LINK
-    },
-
-    {
-      Icon: Reddit,
-      URL: process.env.NEXT_PUBLIC_REDDIT_LINK
-    }
-
-  ]
 
   useEffect(() => {
 
@@ -149,4 +113,4 @@ function Main() {
 
 }
 
-export default Main
+export default Contact
